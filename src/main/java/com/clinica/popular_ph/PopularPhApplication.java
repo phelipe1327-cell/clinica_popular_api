60446af5a5ec52b196f0ee0db53cd490de7a9186
phelipe1327-cell <phelipe1327@gmail.com>
2026-05-18 21:07:08 -0300

    Criação da estrutura do projeto via spring boot

---
+++ src/main/java/com/clinica/popular_ph/PopularPhApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.popular_ph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopularPhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopularPhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinica/popular_ph/PopularPhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.popular_ph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopularPhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
